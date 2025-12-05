3bebb05a4a31122744cc79cef6c885ee5e61c72c
bereketsimma <bekisimma@gmail.com>
2025-12-05 13:54:16 +0300

    setting up registration with django

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,7 +1,9 @@
 class ApiConstants {
-  static const String baseUrl = "https://beautybooking-firl.onrender.com/api/";
-  static const String staff = "${baseUrl}staff/";
-  static const String services = "${baseUrl}services/";
-  static const String bookings = "${baseUrl}bookings/";
-  static const String login = "${baseUrl}login/";
+  static const String baseUrl = "https://beautybooking-firl.onrender.com";
+  static final String staff = "$baseUrl/staff/";
+  static final String services = "$baseUrl/services/";
+  static final String bookings = "$baseUrl/bookings/";
+  static final String login = "$baseUrl/login/";
 }
+
+
